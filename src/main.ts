13a912c4c4cd9d41bda104207e9eed7242829bbc
Sergey Petrov <mavericusdev@gmail.com>
2022-11-05 20:40:26 +0300

    feat: test enviroment varibles heroku

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
-
+const testMessage = process.env.TEST || 'not work';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
